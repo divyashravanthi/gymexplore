a0c6051935f9eefde8e4a59142d0a244f69d1475
Ankit Samarthya <ankitsamar@outlook.com>
2014-12-15 22:08:14 +0530

    enter done

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -602,4 +602,11 @@
 
     $('input, textarea').placeholder();
 
+    $(window).keydown(function(event){
+        if(event.keyCode == 13) {
+          event.preventDefault();
+          return false;
+        }
+    });
+
 })(jQuery);
